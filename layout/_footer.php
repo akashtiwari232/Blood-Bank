33c25c4be49713cb561c204b6fa7755c932a85b3
akashtiwari232 <akashtiwari232@gmail.com>
2021-12-11 23:22:06 +0530

    seventh commit

---
+++ layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="foter">
+            <b>Made By: <a href="">Akash Tiwari</a></b>
+</footer>
+
+
+
+</body>
+</html>
---
+++ users/layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="nav navbar-fixed-bottom">
+            <b>Made By: <a href="">Akash Tiwari</a></b>
+</footer>
+
+
+
+</body>
+</html>
